70c176dd8838e210e9c5e6332120f4ba6ba4c162
arjsr33 <arjunsreedharan32@gmail.com>
2024-06-16 20:59:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+async function main() {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com', 
+    port: 587, 
+    secure: false, 
+    auth: {
+        // Create a .env file and add sender Email address as EMAIL and password as PASSWORD
+      user: process.env.EMAIL, // Your email from .env
+      pass: process.env.PASSWORD, // Your email password from .env
+    },
+  });
+
+  // Setting up email data
+  let mailOptions = {
+    from: `"Arjun K" <${process.env.EMAIL}>`, //Add your Name Here
+    to: 'arjunkolz07@gmail.com', // Enter the To Address here
+    subject: 'NodeMailer Email', // Subject line
+    text: 'This is a Nodemailer Generated Email', // Plain text body
+    html: '<b>This is a Nodemailer Generated Email</b>', // HTML body
+  };
+
+//   Sending Email here
+  let info = await transporter.sendMail(mailOptions);
+  console.log('Message sent: %s', info.messageId);
+}
+main().catch(console.error);
